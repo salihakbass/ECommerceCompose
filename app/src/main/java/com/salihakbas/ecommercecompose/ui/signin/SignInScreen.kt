e72d386fd39f71697358c38ca914b01716d194f9
Salih Akbas <salihhakbass@gmail.com>
2025-01-17 00:34:57 +0300

    Created signIn Screen

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/signin/SignInScreen.kt
@@ -1,14 +1,50 @@
 package com.salihakbas.ecommercecompose.ui.signin
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.salihakbas.ecommercecompose.R
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
 import com.salihakbas.ecommercecompose.ui.components.LoadingBar
 import com.salihakbas.ecommercecompose.ui.signin.SignInContract.UiAction
@@ -32,14 +68,191 @@ fun SignInScreen(
 
 @Composable
 fun SignInContent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "SignIn Content",
+            text = "Welcome Back, Olivia",
             fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Welcome back! Please enter your details.",
+            fontSize = 18.sp,
+            color = Color.Gray
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(
+                    "Email",
+                    color = Color.Gray
+                )
+            },
+
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 24.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp)
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(
+                    "Password",
+                    color = Color.Gray
+                )
+            },
+            modifier = Modifier
+                .padding(horizontal = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {}
+            )
+            Text(
+                text = "Remember me",
+                modifier = Modifier.align(Alignment.CenterVertically),
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "Forgot Password?",
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 12.dp),
+                color = Color.Black
+            )
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            colors = ButtonColors(
+                containerColor = colorResource(R.color.black),
+                contentColor = Color.White,
+                disabledContainerColor = colorResource(R.color.black),
+                disabledContentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "Sign In",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+        Text(
+            text = "or",
+            modifier = Modifier.padding(vertical = 12.dp),
+            color = Color.Gray
+        )
+        GoogleButton(
+            text = "Sign Up with Google",
+            loadingText = "Creating Account...",
+            onClicked = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Gray,
+                    )
+                ) {
+                    append("Don't have an account? ")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("Register")
+                }
+            }
+        )
+
+
+    }
+}
+
+@Composable
+fun GoogleButton(
+    modifier: Modifier,
+    text: String = "Sign Up with Google",
+    loadingText: String = "Creating Account...",
+    icon: Int = R.drawable.ic_google,
+    shape: Shape = MaterialTheme.shapes.medium,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    progressIndicatorColor: Color = MaterialTheme.colorScheme.primary,
+    onClicked: () -> Unit
+) {
+    var clicked by remember { mutableStateOf(false) }
+    Surface(
+        modifier = modifier
+            .clip(MaterialTheme.shapes.medium)
+            .clickable {
+                clicked = !clicked
+                onClicked()
+            },
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = "Google Button",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = if (clicked) loadingText else text
+            )
+
+            if (clicked) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+            }
+        }
     }
 }
 

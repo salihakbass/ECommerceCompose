700cf14ee04a493020475bd02880a51e72ee3d9a
Salih Akbas <salihhakbass@gmail.com>
2025-01-18 16:11:21 +0300

    Added name, surname Textfields and textFields error states

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/signup/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package com.salihakbas.ecommercecompose.ui.signup
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,21 +13,34 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.salihakbas.ecommercecompose.R
-import com.salihakbas.ecommercecompose.ui.components.AnnotatedLoginAndRegisterString
+import com.salihakbas.ecommercecompose.common.collectWithLifecycle
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
 import com.salihakbas.ecommercecompose.ui.components.GoogleButton
 import com.salihakbas.ecommercecompose.ui.components.LoadingBar
@@ -41,16 +55,28 @@ fun SignUpScreen(
     uiState: UiState,
     uiEffect: Flow<UiEffect>,
     onAction: (UiAction) -> Unit,
+    navigateToSignIn: () -> Unit
 ) {
+    val context = LocalContext.current
+    uiEffect.collectWithLifecycle { effect ->
+        when (effect) {
+            is UiEffect.NavigateToSignIn -> navigateToSignIn()
+            is UiEffect.ShowToast -> {
+                Toast.makeText(context, "Kayıt Başarılı", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
     when {
         uiState.isLoading -> LoadingBar()
         uiState.list.isNotEmpty() -> EmptyScreen()
-        else -> SignUpContent()
+        else -> SignUpContent(uiState, onAction, uiEffect)
     }
 }
 
 @Composable
-fun SignUpContent() {
+fun SignUpContent(uiState: UiState, onAction: (UiAction) -> Unit, uiEffect: Flow<UiEffect>) {
+    var isVisible by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -70,47 +96,100 @@ fun SignUpContent() {
             color = Color.Gray
         )
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = uiState.name,
+            onValueChange = { onAction(UiAction.OnNameChange(it)) },
+            label = {
+                Text(
+                    text = stringResource(R.string.name_text),
+                    color = Color.Gray
+                )
+            },
+            modifier = Modifier
+                .padding(start = 12.dp, end = 12.dp, top = 24.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            isError = uiState.errorState?.nameError == true
+        )
+        OutlinedTextField(
+            value = uiState.surname,
+            onValueChange = { onAction(UiAction.OnSurnameChange(it)) },
+            label = {
+                Text(
+                    text = stringResource(R.string.surname_text),
+                    color = Color.Gray
+                )
+            },
+            modifier = Modifier
+                .padding(start = 12.dp, end = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            isError = uiState.errorState?.surnameError == true,
+
+        )
+        OutlinedTextField(
+            value = uiState.email,
+            onValueChange = { onAction(UiAction.OnEmailChange(it)) },
             label = {
                 Text(
                     text = stringResource(R.string.email_text),
                     color = Color.Gray
                 )
             },
+            modifier = Modifier
+                .padding(start = 12.dp, end = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            isError =uiState.errorState?.emailError == true
+        )
+        OutlinedTextField(
+            value = uiState.password,
+            onValueChange = { onAction(UiAction.OnPasswordChange(it)) },
+            label = {
+                Text(
+                    stringResource(R.string.create_password_text),
+                    color = Color.Gray
+                )
+            },
+            modifier = Modifier
+                .padding(horizontal = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = { isVisible = !isVisible }) {
+                    Icon(
+                        painter = painterResource(if (isVisible) R.drawable.ic_visibility_on else R.drawable.ic_visibility_off),
+                        contentDescription = null
+                    )
 
-            modifier = Modifier
-                .padding(start = 12.dp, end = 12.dp, top = 24.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(16.dp)
+                }
+            },
+            isError = uiState.errorState?.passwordError == true
         )
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = uiState.confirmPassword,
+            onValueChange = { onAction(UiAction.OnConfirmPasswordChange(it)) },
             label = {
                 Text(
-                    text = stringResource(R.string.create_password_text),
+                    stringResource(R.string.confirm_password_text),
                     color = Color.Gray
                 )
             },
             modifier = Modifier
-                .padding(start = 12.dp, end = 12.dp, top = 8.dp)
+                .padding(horizontal = 12.dp)
                 .fillMaxWidth(),
-            shape = RoundedCornerShape(16.dp)
-        )
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = {
-                Text(
-                    text = stringResource(R.string.confirm_password_text),
-                    color = Color.Gray
-                )
+            shape = RoundedCornerShape(16.dp),
+            visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = { isVisible = !isVisible }) {
+                    Icon(
+                        painter = painterResource(if (isVisible) R.drawable.ic_visibility_on else R.drawable.ic_visibility_off),
+                        contentDescription = null
+                    )
+
+                }
             },
-            modifier = Modifier
-                .padding(start = 12.dp, end = 12.dp, top = 8.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(16.dp)
+            isError = uiState.errorState?.confirmPasswordError == true
         )
         Row(
             modifier = Modifier
@@ -127,7 +206,7 @@ fun SignUpContent() {
             )
         }
         Button(
-            onClick = {},
+            onClick = { onAction(UiAction.SignUpClick) },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(12.dp),
@@ -159,9 +238,24 @@ fun SignUpContent() {
         )
         Spacer(modifier = Modifier.height(24.dp))
 
-        AnnotatedLoginAndRegisterString(
-            accountText = stringResource(R.string.already_have_an_account_text),
-            loginOrRegisterText = stringResource(R.string.sign_in_text)
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Gray,
+                    )
+                ) {
+                    append(stringResource(R.string.already_have_an_account_text))
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append(stringResource(R.string.sign_in_text))
+                }
+            }
         )
 
 
@@ -177,5 +271,6 @@ fun SignUpScreenPreview(
         uiState = uiState,
         uiEffect = emptyFlow(),
         onAction = {},
+        navigateToSignIn = {}
     )
 }

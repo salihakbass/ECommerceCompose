c977d766bccde4beb4134b907b4284434958c8cb
Salih Akbas <salihhakbass@gmail.com>
2025-01-18 16:12:03 +0300

    Added bindFirebaseAuthRepository

--- app/src/main/java/com/salihakbas/ecommercecompose/di/RepositoryModule.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.salihakbas.ecommercecompose.di
 
+import com.salihakbas.ecommercecompose.data.repository.FirebaseAuthRepositoryImpl
 import com.salihakbas.ecommercecompose.data.repository.MainRepositoryImpl
+import com.salihakbas.ecommercecompose.domain.repository.FirebaseAuthRepository
 import com.salihakbas.ecommercecompose.domain.repository.MainRepository
 import dagger.Binds
 import dagger.Module
@@ -13,4 +15,8 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindMainRepository(repositoryImpl: MainRepositoryImpl): MainRepository
+
+    @Binds
+    abstract fun bindFirebaseAuthRepository(repositoryImpl: FirebaseAuthRepositoryImpl): FirebaseAuthRepository
+
 }

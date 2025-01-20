01205f2cbb00fe7dcdb19e77a550eb2350431cbd
Salih Akbas <salihhakbass@gmail.com>
2025-01-21 02:10:18 +0300

    Created change password states and error states

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/changepassword/ChangePasswordContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/changepassword/ChangePasswordContract.kt
@@ -3,10 +3,49 @@ package com.salihakbas.ecommercecompose.ui.changepassword
 object ChangePasswordContract {
     data class UiState(
         val isLoading: Boolean = false,
+        val oldPassword: String = "",
+        val newPassword: String = "",
+        val newPasswordConfirm: String = "",
+        val isPasswordLongEnough: Boolean = false,
+        val hasNumber: Boolean = false,
+        val hasLetter: Boolean = false,
+        val isPasswordMatching: Boolean = false,
+        val errorState: ErrorState? = null,
         val list: List<String> = emptyList(),
-    )
+    ) {
+        fun checkError(): UiState {
+            return copy(
+                errorState = ErrorState(
+                    passwordError = oldPassword.isEmpty(),
+                    confirmPasswordError = newPassword.isEmpty()
+                )
+            )
+        }
 
-    sealed class UiAction
+        fun isSignUpValid(): Boolean {
+            return isPasswordLongEnough && hasNumber && hasLetter && isPasswordMatching
+        }
+    }
 
-    sealed class UiEffect
+    data class ErrorState(
+        val passwordError: Boolean = false,
+        val confirmPasswordError: Boolean = false
+    ) {
+        fun hasError(): Boolean {
+            return passwordError || confirmPasswordError
+        }
+    }
+
+    sealed class UiAction {
+        data class ChangeOldPassword(val oldPassword: String) : UiAction()
+        data class ChangeNewPassword(val newPassword: String) : UiAction()
+        data class ChangeNewPasswordConfirm(val newPasswordConfirm: String) : UiAction()
+        data object ChangePassword : UiAction()
+    }
+
+    sealed class UiEffect {
+        data object NavigateToProfile : UiEffect()
+        data object ShowErrorToast : UiEffect()
+        data object ShowToast : UiEffect()
+    }
 }

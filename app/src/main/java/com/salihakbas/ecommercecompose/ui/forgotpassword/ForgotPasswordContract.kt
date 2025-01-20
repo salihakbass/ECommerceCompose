c4f93792b4dc315c4652eee35f2c1e7d34017848
Salih Akbas <salihhakbass@gmail.com>
2025-01-20 23:16:32 +0300

    Created UiState,UiAction and UiEffect for ForgotPassword

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/forgotpassword/ForgotPasswordContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/forgotpassword/ForgotPasswordContract.kt
@@ -3,10 +3,19 @@ package com.salihakbas.ecommercecompose.ui.forgotpassword
 object ForgotPasswordContract {
     data class UiState(
         val isLoading: Boolean = false,
+        val email: String = "",
+        val errorMessage: String? = null,
         val list: List<String> = emptyList(),
     )
 
-    sealed class UiAction
+    sealed class UiAction {
+        data class OnEmailChange(val email: String) : UiAction()
+        data object SendCodeClicked : UiAction()
+    }
 
-    sealed class UiEffect
+    sealed class UiEffect {
+        data object NavigateToVerifyCode : UiEffect()
+        data class ShowToast(val message: String) : UiEffect()
+
+    }
 }

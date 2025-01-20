f57305bd64d03416838ce82445c50a0d7575ca88
Salih Akbas <salihhakbass@gmail.com>
2025-01-20 23:17:40 +0300

    Created ChangePasswordScreen

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/changepassword/ChangePasswordScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/changepassword/ChangePasswordScreen.kt
@@ -1,19 +1,35 @@
 package com.salihakbas.ecommercecompose.ui.changepassword
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.salihakbas.ecommercecompose.R
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
 import com.salihakbas.ecommercecompose.ui.components.LoadingBar
 import com.salihakbas.ecommercecompose.ui.changepassword.ChangePasswordContract.UiAction
 import com.salihakbas.ecommercecompose.ui.changepassword.ChangePasswordContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.changepassword.ChangePasswordContract.UiState
+import com.salihakbas.ecommercecompose.ui.components.PasswordRequirements
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
@@ -32,14 +48,82 @@ fun ChangePasswordScreen(
 
 @Composable
 fun ChangePasswordContent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 132.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "ChangePassword Content",
+            text = "Yeni Şifre Oluştur",
             fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Tekrar hoşgeldiniz. Lütfen yeni şifrenizi giriniz.",
+            textAlign = TextAlign.Center,
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(horizontal = 24.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 24.dp)
+                .fillMaxWidth(),
+            label = {
+                Text(
+                    text = "Yeni şifre oluştur",
+                    color = Color.Gray
+                )
+            },
+            shape = RoundedCornerShape(16.dp)
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            label = {
+                Text(
+                    text = "Yeni şifreyi doğrula",
+                    color = Color.Gray
+                )
+            },
+            shape = RoundedCornerShape(16.dp)
+        )
+
+        PasswordRequirements(
+            isPasswordLongEnough = true,
+            hasLetter = true,
+            hasPassword = true,
+            isPasswordMatching = true
+        )
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            colors = ButtonColors(
+                containerColor = colorResource(R.color.black),
+                contentColor = Color.White,
+                disabledContainerColor = colorResource(R.color.black),
+                disabledContentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "Şifreyi Oluştur",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
     }
 }
 

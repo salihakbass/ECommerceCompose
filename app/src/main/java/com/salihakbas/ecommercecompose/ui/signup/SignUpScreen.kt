65a6c622a0ab8d72d5fe1bf868877677f5920e10
Salih Akbas <salihhakbass@gmail.com>
2025-01-21 02:13:14 +0300

    Removed passwordRequirementsText

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/signup/SignUpScreen.kt
@@ -29,12 +29,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -44,6 +41,7 @@ import com.salihakbas.ecommercecompose.common.collectWithLifecycle
 import com.salihakbas.ecommercecompose.ui.components.AuthAnnotatedText
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
 import com.salihakbas.ecommercecompose.ui.components.LoadingBar
+import com.salihakbas.ecommercecompose.ui.components.PasswordRequirements
 import com.salihakbas.ecommercecompose.ui.signup.SignUpContract.UiAction
 import com.salihakbas.ecommercecompose.ui.signup.SignUpContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.signup.SignUpContract.UiState
@@ -248,48 +246,9 @@ fun SignUpContent(uiState: UiState, onAction: (UiAction) -> Unit) {
     }
 }
 
-@Composable
-fun PasswordRequirements(
-    isPasswordLongEnough: Boolean,
-    hasLetter: Boolean,
-    hasPassword: Boolean,
-    isPasswordMatching: Boolean
-) {
-    Column(
-        modifier = Modifier
-            .padding(12.dp)
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.Start
-    ) {
-        PasswordRequirement(
-            text = stringResource(R.string.password_must_six_text),
-            isMet = isPasswordLongEnough
-        )
-        PasswordRequirement(
-            text = stringResource(R.string.password_must_has_letter_text),
-            isMet = hasLetter
-        )
-        PasswordRequirement(
-            text = stringResource(R.string.password_must_has_number_text),
-            isMet = hasPassword
-        )
-        PasswordRequirement(
-            text = stringResource(R.string.password_must_match_text),
-            isMet = isPasswordMatching
-        )
-    }
-}
 
-@Composable
-fun PasswordRequirement(text: String, isMet: Boolean) {
-    Text(
-        text = text,
-        fontSize = 12.sp,
-        color = if (isMet) Color.Green else Color.Red,
-        fontWeight = if (isMet) FontWeight.Bold else FontWeight.Normal,
-        modifier = Modifier.padding(vertical = 4.dp)
-    )
-}
+
+
 
 @Preview(showBackground = true)
 @Composable

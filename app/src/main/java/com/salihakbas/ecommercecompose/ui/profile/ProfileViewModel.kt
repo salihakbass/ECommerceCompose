68d62245dfce68bf5fe22195f15fc4d7c943ea0d
Salih Akbas <salihhakbass@gmail.com>
2025-01-21 02:12:33 +0300

    Added signOut and changePassword functions

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/profile/ProfileViewModel.kt
@@ -1,6 +1,9 @@
 package com.salihakbas.ecommercecompose.ui.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salihakbas.ecommercecompose.common.Resource
+import com.salihakbas.ecommercecompose.domain.repository.FirebaseAuthRepository
 import com.salihakbas.ecommercecompose.ui.profile.ProfileContract.UiAction
 import com.salihakbas.ecommercecompose.ui.profile.ProfileContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.profile.ProfileContract.UiState
@@ -12,10 +15,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
@@ -24,6 +30,26 @@ class ProfileViewModel @Inject constructor() : ViewModel() {
     val uiEffect: Flow<UiEffect> by lazy { _uiEffect.receiveAsFlow() }
 
     fun onAction(uiAction: UiAction) {
+        when (uiAction) {
+            UiAction.SignOutClicked -> signOut()
+            UiAction.ChangePasswordClicked -> changePassword()
+        }
+    }
+
+    private fun changePassword() = viewModelScope.launch {
+        emitUiEffect(UiEffect.NavigateToChangePassword)
+    }
+
+    private fun signOut() = viewModelScope.launch {
+        when (val result = authRepository.signOut()) {
+            is Resource.Success -> {
+                emitUiEffect(UiEffect.NavigateToSignIn)
+            }
+
+            is Resource.Error -> {
+                // Handle error
+            }
+        }
     }
 
     private fun updateUiState(block: UiState.() -> UiState) {

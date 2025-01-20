eef3861fd1b04bb419de8eebc3fb6572223e5c13
Salih Akbas <salihhakbass@gmail.com>
2025-01-21 02:11:01 +0300

    Added change password and validate password functions

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/changepassword/ChangePasswordViewModel.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/changepassword/ChangePasswordViewModel.kt
@@ -1,6 +1,9 @@
 package com.salihakbas.ecommercecompose.ui.changepassword
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salihakbas.ecommercecompose.common.Resource
+import com.salihakbas.ecommercecompose.domain.repository.FirebaseAuthRepository
 import com.salihakbas.ecommercecompose.ui.changepassword.ChangePasswordContract.UiAction
 import com.salihakbas.ecommercecompose.ui.changepassword.ChangePasswordContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.changepassword.ChangePasswordContract.UiState
@@ -12,10 +15,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ChangePasswordViewModel @Inject constructor() : ViewModel() {
+class ChangePasswordViewModel @Inject constructor(
+    private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
@@ -24,6 +30,68 @@ class ChangePasswordViewModel @Inject constructor() : ViewModel() {
     val uiEffect: Flow<UiEffect> by lazy { _uiEffect.receiveAsFlow() }
 
     fun onAction(uiAction: UiAction) {
+        when (uiAction) {
+            is UiAction.ChangeOldPassword -> updateUiState { copy(oldPassword = uiAction.oldPassword) }
+
+            is UiAction.ChangeNewPassword -> {
+                updateUiState { copy(newPassword = uiAction.newPassword) }
+                validatePassword(uiAction.newPassword, uiState.value.newPasswordConfirm)
+            }
+
+            is UiAction.ChangeNewPasswordConfirm -> {
+                updateUiState { copy(newPasswordConfirm = uiAction.newPasswordConfirm) }
+                validatePassword(uiAction.newPasswordConfirm, uiState.value.newPassword)
+            }
+
+            UiAction.ChangePassword -> changePassword()
+        }
+    }
+
+    private fun changePassword() = viewModelScope.launch {
+        updateUiState { checkError() }
+        val currentUserEmail = authRepository.getCurrentUserEmail()
+        if (currentUserEmail.isNullOrEmpty()) {
+            emitUiEffect(UiEffect.ShowErrorToast)
+            return@launch
+        }
+        if (uiState.value.errorState?.hasError() == true) {
+            emitUiEffect(UiEffect.ShowErrorToast)
+            return@launch
+        }
+
+        if (!uiState.value.isSignUpValid()) {
+            emitUiEffect(UiEffect.ShowErrorToast)
+            return@launch
+        }
+        authRepository.changePassword(
+            email = currentUserEmail,
+            oldPassword = uiState.value.oldPassword,
+            newPassword = uiState.value.newPassword
+        ).also { result ->
+            when (result) {
+                is Resource.Success -> {
+                    emitUiEffect(UiEffect.ShowToast)
+                    emitUiEffect(UiEffect.NavigateToProfile)
+                }
+
+                is Resource.Error -> {
+                    emitUiEffect(UiEffect.ShowErrorToast)
+                }
+            }
+        }
+    }
+
+    private fun validatePassword(password: String, confirmPassword: String) {
+        val containsLetter = password.any { it.isLetter() }
+        val containsDigit = password.any { it.isDigit() }
+        updateUiState {
+            copy(
+                isPasswordLongEnough = password.length >= 6,
+                hasLetter = containsLetter,
+                hasNumber = containsDigit,
+                isPasswordMatching = password == confirmPassword
+            )
+        }
     }
 
     private fun updateUiState(block: UiState.() -> UiState) {

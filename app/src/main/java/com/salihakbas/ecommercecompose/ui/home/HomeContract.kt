a943d78cef7cadc5a00cce333ec3dcc6c695ebda
Salih Akbas <salihhakbass@gmail.com>
2025-01-22 16:44:14 +0300

    Added username,usersurname,productlist,categorylist uiStates and fetchProducts UiAction

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/home/HomeContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/home/HomeContract.kt
@@ -1,12 +1,23 @@
 package com.salihakbas.ecommercecompose.ui.home
 
+import com.salihakbas.ecommercecompose.data.source.remote.Category
+import com.salihakbas.ecommercecompose.domain.model.Product
+
 object HomeContract {
     data class UiState(
         val isLoading: Boolean = false,
         val list: List<String> = emptyList(),
+        val userName: String = "",
+        val userSurname: String = "",
+        val productList: List<Product> = emptyList(),
+        val categoryList:List<Category> = emptyList(),
+        val errorMessage: String? = null
     )
 
-    sealed class UiAction
+    sealed class UiAction {
+
+        data object FetchProducts : UiAction()
+    }
 
     sealed class UiEffect
 }

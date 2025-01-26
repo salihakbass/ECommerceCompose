95859b179ebacac7c4f03d84210f2b64048a545e
Salih Akbas <salihhakbass@gmail.com>
2025-01-26 22:44:59 +0300

    Added horizontalPager for discounted products

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/home/HomeScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/home/HomeScreen.kt
@@ -1,7 +1,11 @@
 package com.salihakbas.ecommercecompose.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,19 +15,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.salihakbas.ecommercecompose.domain.model.Product
 import com.salihakbas.ecommercecompose.ui.components.CategoryList
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
@@ -50,7 +57,6 @@ fun HomeScreen(
             uiState = uiState,
             onCategoryClick = onCategoryClick,
             onAction = onAction
-
         )
     }
 }
@@ -61,20 +67,36 @@ fun HomeContent(
     onCategoryClick: (String?) -> Unit,
     onAction: (UiAction) -> Unit
 ) {
-
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(8.dp)
     ) {
+        Text(
+            text = "Hoşgeldin, ${uiState.userName}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 8.dp, top = 16.dp)
+        )
         SearchView(
             query = uiState.query,
-            onQueryChange = { onAction(UiAction.OnQueryChanged(it)) },
-            onSearchQuery = {query ->
-                onAction(UiAction.SearchProducts(query))
-            }
+            onQueryChange = { onAction(UiAction.OnQueryChanged(it)) }
         )
-
+        Text(
+            text = "İndirimli Ürünler",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 8.dp, top = 16.dp)
+        )
+        DiscountedProductsPager(uiState.productList)
+        Text(
+            text = "Kategoriler",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 8.dp, top = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
         CategoryList(
             uiState = uiState,
             onCategoryClick = onCategoryClick
@@ -107,8 +129,77 @@ fun ProductCard(product: Product) {
             contentDescription = product.title,
             modifier = Modifier.size(150.dp)
         )
-        Text(text = product.title, fontSize = 18.sp)
-        Text(text = "₺${product.price}", fontSize = 16.sp)
+        Text(
+            text = product.title,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            maxLines = 2
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "₺${product.price}",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        if (product.saleState) {
+            Text(
+                text = "₺${product.salePrice}",
+            )
+        }
+    }
+}
+
+
+@Composable
+fun DiscountedProductsPager(products: List<Product>) {
+    val discountedProducts = products.filter { it.saleState }
+    val pagerState = rememberPagerState(pageCount = {discountedProducts.size})
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+        ) { page ->
+            val product = discountedProducts[page]
+            ProductCard(product = product)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DotsIndicator(
+            totalDots = discountedProducts.size,
+            selectedIndex = pagerState.currentPage
+        )
+    }
+}
+
+@Composable
+fun DotsIndicator(
+    totalDots: Int,
+    selectedIndex: Int
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        repeat(totalDots) { index ->
+            Box(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(if (index == selectedIndex) 12.dp else 8.dp)
+                    .background(
+                        color = if (index == selectedIndex) androidx.compose.ui.graphics.Color.Gray
+                        else androidx.compose.ui.graphics.Color.LightGray,
+                        shape = androidx.compose.foundation.shape.CircleShape
+                    )
+            )
+        }
     }
 }
 

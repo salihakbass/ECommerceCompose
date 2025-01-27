99ccb4b508aaf68e10ea406bf4bcfcdd7a95cdd6
Salih Akbas <salihhakbass@gmail.com>
2025-01-27 21:08:33 +0300

    Created search screen ui

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/search/SearchScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/search/SearchScreen.kt
@@ -1,17 +1,39 @@
 package com.salihakbas.ecommercecompose.ui.search
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.salihakbas.ecommercecompose.ui.cart.CartContract
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
 import com.salihakbas.ecommercecompose.ui.components.LoadingBar
+import com.salihakbas.ecommercecompose.ui.components.SearchTextField
+import com.salihakbas.ecommercecompose.ui.components.SuggestedProductsPager
 import com.salihakbas.ecommercecompose.ui.search.SearchContract.UiAction
 import com.salihakbas.ecommercecompose.ui.search.SearchContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.search.SearchContract.UiState
@@ -27,27 +49,100 @@ fun SearchScreen(
     when {
         uiState.isLoading -> LoadingBar()
         uiState.list.isNotEmpty() -> EmptyScreen()
-        else -> SearchContent()
+        else -> SearchContent(
+            uiState = uiState,
+            onAction = onAction
+        )
     }
 }
 
 @Composable
-fun SearchContent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+fun SearchContent(
+    uiState: UiState,
+    onAction: (UiAction) -> Unit
+) {
+    val popularItems = listOf("Msi", "Asus", "Lenovo", "SteelSeries", "Razer")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Text(
-            text = "Cart Content",
-            fontSize = 24.sp,
+        SearchTextField(
+            query = uiState.query,
+            onQueryChange = { onAction(UiAction.OnQueryChanged(it)) },
+            navigateToSearch = {}
         )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+
+        ) {
+            items(popularItems) { item ->
+                Text(
+                    text = item,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(12.dp))
+                        .border(
+                            1.dp,
+                            Color.Gray,
+                            RoundedCornerShape(12.dp)
+                        )
+                        .padding(horizontal = 24.dp, vertical = 8.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (uiState.query.isNotEmpty()) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(uiState.allProducts) { product ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(product.imageOne).build(),
+                            contentDescription = product.title,
+                            modifier = Modifier.size(150.dp)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text = product.title,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.CenterVertically)
+                        )
+                    }
+
+                }
+            }
+        } else {
+            SuggestedProductsPager(
+                products = uiState.suggestedProducts,
+            )
+        }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SearchScreenPreview(
-    @PreviewParameter(SearchScreenPreviewProvider::class) uiState: SearchContract.UiState,
+    @PreviewParameter(SearchScreenPreviewProvider::class) uiState: UiState,
 ) {
     SearchScreen(
         uiState = uiState,

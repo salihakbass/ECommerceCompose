5441b5b8fcf6414d92d27189554cceab59af9b14
Salih Akbas <salihhakbass@gmail.com>
2025-01-27 21:08:10 +0300

    Added suggested and all products, query states and onQueryChanged action

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/search/SearchContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/search/SearchContract.kt
@@ -1,12 +1,19 @@
 package com.salihakbas.ecommercecompose.ui.search
 
+import com.salihakbas.ecommercecompose.domain.model.Product
+
 object SearchContract {
     data class UiState(
         val isLoading: Boolean = false,
         val list: List<String> = emptyList(),
+        val suggestedProducts: List<Product> = emptyList(),
+        val allProducts: List<Product> = emptyList(),
+        val query: String = "",
     )
 
-    sealed class UiAction
+    sealed class UiAction {
+        data class OnQueryChanged(val query: String) : UiAction()
+    }
 
     sealed class UiEffect
 }

71a41c30e8d45f480c8c619889fd57eb624665f5
Salih Akbas <salihhakbass@gmail.com>
2025-01-27 21:07:13 +0300

    Created SuggestedProductsCard

---
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/components/SuggestedProductsCard.kt
@@ -0,0 +1,122 @@
+package com.salihakbas.ecommercecompose.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.salihakbas.ecommercecompose.domain.model.Product
+import com.salihakbas.ecommercecompose.ui.home.DotsIndicator
+
+@Composable
+fun SuggestedProductsCard(
+    product: Product,
+    onProductClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current).data(product.imageOne).build(),
+            contentDescription = product.title,
+            modifier = Modifier.fillMaxWidth().size(250.dp)
+        )
+        Text(
+            text = product.title,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.padding(horizontal = 8.dp),
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = "₺${product.price}",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+        }
+    }
+}
+
+@Composable
+fun SuggestedProductsPager(products: List<Product>) {
+    val suggestedProducts = remember {
+        products.filter { !it.saleState }.shuffled().take(5)
+    }
+    val pagerState = rememberPagerState(pageCount = { suggestedProducts.size })
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Senin İçin Önerilenler",
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.Start),
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textDecoration = TextDecoration.Underline
+            ),
+        )
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+        ) { page ->
+            val product = suggestedProducts[page]
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    SuggestedProductsCard(
+                        product = product,
+                        onProductClick = {}
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DotsIndicator(
+            totalDots = suggestedProducts.size,
+            selectedIndex = pagerState.currentPage
+        )
+    }
+}

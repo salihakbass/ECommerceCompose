8ef2241c9632a79c431760547dbf378989f4fd9e
Salih Akbas <salihhakbass@gmail.com>
2025-01-28 19:08:22 +0300

    Created discount classes

---
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/discount/DiscountViewModel.kt
@@ -0,0 +1,33 @@
+package com.salihakbas.ecommercecompose.ui.discount
+
+import androidx.lifecycle.ViewModel
+import com.salihakbas.ecommercecompose.ui.discount.DiscountContract.UiAction
+import com.salihakbas.ecommercecompose.ui.discount.DiscountContract.UiEffect
+import com.salihakbas.ecommercecompose.ui.discount.DiscountContract.UiState
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+
+class DiscountViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    private val _uiEffect by lazy { Channel<UiEffect>() }
+    val uiEffect: Flow<UiEffect> by lazy { _uiEffect.receiveAsFlow() }
+
+    fun onAction(uiAction: UiAction) {
+    }
+
+    private fun updateUiState(block: UiState.() -> UiState) {
+        _uiState.update(block)
+    }
+
+    private suspend fun emitUiEffect(uiEffect: UiEffect) {
+        _uiEffect.send(uiEffect)
+    }
+}

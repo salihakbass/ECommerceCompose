718a3daba6fa9859db185c45b2ae0f9a3f2c579a
Salih Akbas <salihhakbass@gmail.com>
2025-02-02 23:06:10 +0300

    Added fetchProducts function

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/product/ProductViewModel.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/product/ProductViewModel.kt
@@ -1,6 +1,9 @@
 package com.salihakbas.ecommercecompose.ui.product
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salihakbas.ecommercecompose.common.Resource
+import com.salihakbas.ecommercecompose.domain.usecase.FetchProductsUseCase
 import com.salihakbas.ecommercecompose.ui.product.ProductContract.UiAction
 import com.salihakbas.ecommercecompose.ui.product.ProductContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.product.ProductContract.UiState
@@ -12,10 +15,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProductViewModel @Inject constructor() : ViewModel() {
+class ProductViewModel @Inject constructor(
+    private val fetchProductsUseCase: FetchProductsUseCase
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
@@ -23,9 +29,30 @@ class ProductViewModel @Inject constructor() : ViewModel() {
     private val _uiEffect by lazy { Channel<UiEffect>() }
     val uiEffect: Flow<UiEffect> by lazy { _uiEffect.receiveAsFlow() }
 
+    init {
+        fetchProducts()
+    }
 
     fun onAction(uiAction: UiAction) {
 
+    }
+
+    private fun fetchProducts() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        when (val result = fetchProductsUseCase()) {
+            is Resource.Success -> {
+                updateUiState {
+                    copy(
+                        isLoading = false,
+                        productList = result.data
+                    )
+                }
+            }
+
+            is Resource.Error -> {
+                updateUiState { copy(isLoading = false) }
+            }
+        }
     }
 
     private fun updateUiState(block: UiState.() -> UiState) {

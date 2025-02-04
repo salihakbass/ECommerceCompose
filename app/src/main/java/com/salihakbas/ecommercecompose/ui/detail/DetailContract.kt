3d0d1a3b991992c5e8d351b45cc1a2756ddd1237
Salih Akbas <salihhakbass@gmail.com>
2025-02-04 14:00:04 +0300

    Added product and similarProduct uiState

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/detail/DetailContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/detail/DetailContract.kt
@@ -1,9 +1,15 @@
 package com.salihakbas.ecommercecompose.ui.detail
 
+import com.salihakbas.ecommercecompose.domain.model.Product
+import com.salihakbas.ecommercecompose.domain.model.ProductDetail
+
 object DetailContract {
     data class UiState(
         val isLoading: Boolean = false,
         val list: List<String> = emptyList(),
+        val error: String = "",
+        val product: ProductDetail? = null,
+        val similarProducts: List<Product> = emptyList()
     )
 
     sealed class UiAction

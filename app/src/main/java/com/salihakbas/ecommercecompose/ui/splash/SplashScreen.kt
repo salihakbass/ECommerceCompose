4cec2f0cb6a78edf4f8cafb3989ba52c7b10ce65
Salih Akbas <salihhakbass@gmail.com>
2025-02-09 17:19:07 +0300

    Added isUserLoggedIn control

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/splash/SplashScreen.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.salihakbas.ecommercecompose.navigation.Screen
 import com.salihakbas.ecommercecompose.ui.components.EmptyScreen
 import com.salihakbas.ecommercecompose.ui.components.LoadingBar
 import com.salihakbas.ecommercecompose.ui.splash.SplashContract.UiAction
 import com.salihakbas.ecommercecompose.ui.splash.SplashContract.UiEffect
 import com.salihakbas.ecommercecompose.ui.splash.SplashContract.UiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
@@ -22,35 +28,39 @@ fun SplashScreen(
     uiState: UiState,
     uiEffect: Flow<UiEffect>,
     onAction: (UiAction) -> Unit,
+    navController: NavController
 ) {
     when {
         uiState.isLoading -> LoadingBar()
         uiState.list.isNotEmpty() -> EmptyScreen()
-        else -> SplashContent()
+        else -> SplashContent(navController)
     }
 }
 
 @Composable
-fun SplashContent() {
+fun SplashContent(navController: NavController) {
+    val auth = FirebaseAuth.getInstance()
+
+    LaunchedEffect(Unit) {
+        delay(2000) // Splash ekranı için kısa bir bekleme süresi
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            navController.navigate(Screen.getRoute(Screen.Home)) {
+                popUpTo(Screen.getRoute(Screen.Splash)) { inclusive = true }
+            }
+        } else {
+            navController.navigate(Screen.getRoute(Screen.SignIn)) {
+                popUpTo(Screen.getRoute(Screen.Splash)) { inclusive = true }
+            }
+        }
+    }
+
+    // Burada Splash ekranın UI tasarımını oluşturabilirsiniz
     Box(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Splash Content",
-            fontSize = 24.sp,
-        )
+        Text("Loading...", fontSize = 24.sp)
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun SplashScreenPreview(
-    @PreviewParameter(SplashScreenPreviewProvider::class) uiState: UiState,
-) {
-    SplashScreen(
-        uiState = uiState,
-        uiEffect = emptyFlow(),
-        onAction = {},
-    )
-}

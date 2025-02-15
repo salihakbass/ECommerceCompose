7e611d24019e5c8e45e1761d9e8a4d7dfbd5668d
Salih Akbas <salihhakbass@gmail.com>
2025-02-15 13:49:01 +0300

    Added uiState and UiAction

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutContract.kt
@@ -1,2 +1,32 @@
 package com.salihakbas.ecommercecompose.ui.checkout
 
+import com.salihakbas.ecommercecompose.data.model.Address
+
+object CheckoutContract {
+
+    data class UiState(
+        val isLoading: Boolean = false,
+        val list: List<String> = emptyList(),
+        val error: String = "",
+        val totalPrice: Double = 0.0,
+        val address: List<Address> = emptyList(),
+        val title: String = "",
+        val description: String = "",
+        val city: String = "",
+        val district: String = "",
+        val code: String = ""
+    )
+
+    sealed interface UiAction {
+        data class AddAddress(val address: Address) : UiAction
+        data class OnTitleChange(val title: String) : UiAction
+        data class OnDescriptionChange(val description: String) : UiAction
+        data class OnCityChange(val city: String) : UiAction
+        data class OnDistrictChange(val district: String) : UiAction
+        data class OnCodeChange(val code: String) : UiAction
+    }
+
+    sealed interface UiEffect
+
+
+}

bad4f95dc8c8e88f2f1854e9d28e32075d4b9971
Salih Akbas <salihhakbass@gmail.com>
2025-02-15 13:50:06 +0300

    Created insert and getAddress room functions

--- app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainDao.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainDao.kt
@@ -1,6 +1,17 @@
 package com.salihakbas.ecommercecompose.data.source.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.salihakbas.ecommercecompose.data.model.Address
 
 @Dao
-interface MainDao
+interface MainDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAddress(address: Address)
+
+    @Query("SELECT * FROM address")
+    suspend fun getAllAddress(): List<Address>
+}

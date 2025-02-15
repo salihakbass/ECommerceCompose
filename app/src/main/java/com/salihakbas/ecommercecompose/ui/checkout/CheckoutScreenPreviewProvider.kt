b973b1c92675915f742cfe53308b31f89320f835
Salih Akbas <salihhakbass@gmail.com>
2025-02-15 13:50:43 +0300

    Added Address preview

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutScreenPreviewProvider.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutScreenPreviewProvider.kt
@@ -1,2 +1,30 @@
 package com.salihakbas.ecommercecompose.ui.checkout
 
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.salihakbas.ecommercecompose.data.model.Address
+
+class CheckoutScreenPreviewProvider : PreviewParameterProvider<CheckoutContract.UiState> {
+    override val values: Sequence<CheckoutContract.UiState>
+        get() = sequenceOf(
+            CheckoutContract.UiState(
+                isLoading = true,
+                list = emptyList(),
+            ),
+            CheckoutContract.UiState(
+                isLoading = false,
+                list = emptyList(),
+            ),
+            CheckoutContract.UiState(
+                isLoading = false,
+                address = listOf(
+                    Address(
+                        title = "Ev",
+                        description = "Karadeniz Caddesi Sahil Mahallesi",
+                        city = "Trabzon",
+                        district = "Araklı",
+                        code = "61000"
+                    )
+                )
+            ),
+        )
+}

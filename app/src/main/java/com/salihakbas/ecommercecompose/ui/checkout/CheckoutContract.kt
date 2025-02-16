82207ad0a2a918e5ca580c5ac88dbdaff1c2f591
Salih Akbas <salihhakbass@gmail.com>
2025-02-16 21:24:09 +0300

    Added creditCard uiAction and uiState

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutContract.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutContract.kt
@@ -1,6 +1,7 @@
 package com.salihakbas.ecommercecompose.ui.checkout
 
 import com.salihakbas.ecommercecompose.data.model.Address
+import com.salihakbas.ecommercecompose.data.model.CreditCard
 
 object CheckoutContract {
 
@@ -10,20 +11,32 @@ object CheckoutContract {
         val error: String = "",
         val totalPrice: Double = 0.0,
         val address: List<Address> = emptyList(),
+        val creditCards: List<CreditCard> = emptyList(),
         val title: String = "",
         val description: String = "",
         val city: String = "",
         val district: String = "",
-        val code: String = ""
+        val code: String = "",
+        val cardName: String = "",
+        val cardNumber: String = "",
+        val cardDate: String = "",
+        val cardCvv: String = "",
+        val cardOwnerName: String = ""
     )
 
     sealed interface UiAction {
         data class AddAddress(val address: Address) : UiAction
+        data class AddCreditCard(val creditCard: CreditCard) : UiAction
         data class OnTitleChange(val title: String) : UiAction
         data class OnDescriptionChange(val description: String) : UiAction
         data class OnCityChange(val city: String) : UiAction
         data class OnDistrictChange(val district: String) : UiAction
         data class OnCodeChange(val code: String) : UiAction
+        data class OnCardNameChange(val cardName: String) : UiAction
+        data class OnCardNumberChange(val cardNumber: String) : UiAction
+        data class OnCardDateChange(val cardDate: String) : UiAction
+        data class OnCardCvvChange(val cardCvv: String) : UiAction
+        data class OnCardOwnerNameChange(val cardOwnerName: String) : UiAction
     }
 
     sealed interface UiEffect

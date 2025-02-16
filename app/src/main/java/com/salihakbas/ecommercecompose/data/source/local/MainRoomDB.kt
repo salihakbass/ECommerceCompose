41e374d0a267673bcc688ca3093a0d825ac54ba4
Salih Akbas <salihhakbass@gmail.com>
2025-02-16 21:25:54 +0300

    Added creditCard entity

--- app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainRoomDB.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainRoomDB.kt
@@ -2,9 +2,11 @@ package com.salihakbas.ecommercecompose.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.salihakbas.ecommercecompose.data.model.Address
+import com.salihakbas.ecommercecompose.data.model.CreditCard
 import com.salihakbas.ecommercecompose.data.model.MainEntityModel
 
-@Database(entities = [MainEntityModel::class], version = 1, exportSchema = false)
+@Database(entities = [Address::class,CreditCard::class], version = 3, exportSchema = false)
 abstract class MainRoomDB : RoomDatabase() {
     abstract fun mainDao(): MainDao
 }

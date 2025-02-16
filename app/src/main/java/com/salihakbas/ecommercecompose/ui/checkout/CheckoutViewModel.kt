ec4cb9e30ecc21a0f67345a0f4781d2e0f175038
Salih Akbas <salihhakbass@gmail.com>
2025-02-16 21:24:37 +0300

    Added creditCard uiAction and load, add functions

--- app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/ui/checkout/CheckoutViewModel.kt
@@ -1,9 +1,9 @@
 package com.salihakbas.ecommercecompose.ui.checkout
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.salihakbas.ecommercecompose.data.model.Address
+import com.salihakbas.ecommercecompose.data.model.CreditCard
 import com.salihakbas.ecommercecompose.data.source.local.MainDao
 import com.salihakbas.ecommercecompose.ui.checkout.CheckoutContract.UiAction
 import com.salihakbas.ecommercecompose.ui.checkout.CheckoutContract.UiEffect
@@ -32,16 +32,23 @@ class CheckoutViewModel @Inject constructor(
 
     init {
         loadAddresses()
+        loadCreditCards()
     }
 
     fun onAction(uiAction: UiAction) {
         when (uiAction) {
             is UiAction.AddAddress -> addAddress(uiAction.address)
+            is UiAction.AddCreditCard -> addCreditCard(uiAction.creditCard)
             is UiAction.OnTitleChange -> updateUiState { copy(title = uiAction.title) }
             is UiAction.OnDescriptionChange -> updateUiState { copy(description = uiAction.description) }
             is UiAction.OnCityChange -> updateUiState { copy(city = uiAction.city) }
             is UiAction.OnDistrictChange -> updateUiState { copy(district = uiAction.district) }
             is UiAction.OnCodeChange -> updateUiState { copy(code = uiAction.code) }
+            is UiAction.OnCardNameChange -> updateUiState { copy(cardName = uiAction.cardName) }
+            is UiAction.OnCardNumberChange -> updateUiState { copy(cardNumber = uiAction.cardNumber) }
+            is UiAction.OnCardDateChange -> updateUiState { copy(cardDate = uiAction.cardDate) }
+            is UiAction.OnCardCvvChange -> updateUiState { copy(cardCvv = uiAction.cardCvv) }
+            is UiAction.OnCardOwnerNameChange -> updateUiState { copy(cardOwnerName = uiAction.cardOwnerName) }
         }
     }
 
@@ -50,6 +57,21 @@ class CheckoutViewModel @Inject constructor(
             mainDao.insertAddress(address)
             val addresses = mainDao.getAllAddress()
             updateUiState { copy(address = addresses) }
+        }
+    }
+
+    private fun addCreditCard(creditCard: CreditCard) {
+        viewModelScope.launch {
+            mainDao.insertCreditCard(creditCard)
+            val creditCards = mainDao.getAllCreditCards()
+            updateUiState { copy(creditCards = creditCards) }
+        }
+    }
+
+    private fun loadCreditCards() {
+        viewModelScope.launch {
+            val creditCards = mainDao.getAllCreditCards()
+            updateUiState { copy(creditCards = creditCards) }
         }
     }
 

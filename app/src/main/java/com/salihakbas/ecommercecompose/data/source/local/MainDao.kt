b7527b6737278d1b99fe5553723948af538e7c8c
Salih Akbas <salihhakbass@gmail.com>
2025-02-17 20:32:46 +0300

    Added insert,delete and getFavorite functions

--- app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainDao.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainDao.kt
@@ -6,6 +6,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.salihakbas.ecommercecompose.data.model.Address
 import com.salihakbas.ecommercecompose.data.model.CreditCard
+import com.salihakbas.ecommercecompose.data.model.FavoriteProduct
 
 @Dao
 interface MainDao {
@@ -20,4 +21,17 @@ interface MainDao {
 
     @Query("SELECT * FROM creditCard")
     suspend fun getAllCreditCards(): List<CreditCard>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteProduct(favoriteProduct: FavoriteProduct)
+
+    @Query("DELETE FROM favorite_products WHERE id = :productId")
+    suspend fun removeFavoriteProduct(productId: Int)
+
+    @Query("SELECT * FROM favorite_products")
+    suspend fun getAllFavoriteProducts(): List<FavoriteProduct>
+
+    @Query("SELECT EXISTS (SELECT 1 FROM favorite_products WHERE id = :productId)")
+    suspend fun isFavoriteProduct(productId: Int): Boolean
+
 }

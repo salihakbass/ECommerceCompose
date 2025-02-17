3df53b1b2c8bcb550c88dc18d25125ab19a97216
Salih Akbas <salihhakbass@gmail.com>
2025-02-17 20:35:14 +0300

    Added FavoriteProduct class to entities

--- app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainRoomDB.kt
+++ app/src/main/java/com/salihakbas/ecommercecompose/data/source/local/MainRoomDB.kt
@@ -4,9 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.salihakbas.ecommercecompose.data.model.Address
 import com.salihakbas.ecommercecompose.data.model.CreditCard
-import com.salihakbas.ecommercecompose.data.model.MainEntityModel
+import com.salihakbas.ecommercecompose.data.model.FavoriteProduct
 
-@Database(entities = [Address::class,CreditCard::class], version = 3, exportSchema = false)
+@Database(
+    entities = [Address::class, CreditCard::class, FavoriteProduct::class],
+    version = 4,
+    exportSchema = false
+)
 abstract class MainRoomDB : RoomDatabase() {
     abstract fun mainDao(): MainDao
 }
